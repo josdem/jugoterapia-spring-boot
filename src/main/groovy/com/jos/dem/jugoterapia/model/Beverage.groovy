93e8978aae2caf82f73cbd2df52bfb23168bcd08
josdem <joseluis.delacruz@gmail.com>
2016-05-11 16:06:18 -0500

    Adding beverage and category mapping

--- src/main/groovy/com/jos/dem/jugoterapia/model/Beverage.groovy
+++ src/main/groovy/com/jos/dem/jugoterapia/model/Beverage.groovy
@@ -3,18 +3,22 @@ package com.jos.dem.jugoterapia.model
 import javax.persistence.Entity
 import javax.persistence.Id
 import javax.persistence.Column
-import javax.persistence.OneToMany
+import javax.persistence.ManyToOne
 
 @Entity
-class Category {
+class Beverage {
 
   @Id
   Long id
 
   @Column(nullable = false)
   String name
+  @Column(nullable = false)
+  String ingredients
+  @Column(nullable = false)
+  String recipe
 
-  @OneToMany
-  List<Beverage> beverages
+  @ManyToOne
+  Category category
 
 }
