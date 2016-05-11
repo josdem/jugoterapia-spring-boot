37471a92571626cfdef526ad01551ea4bac51caf
josdem <joseluis.delacruz@gmail.com>
2016-05-11 14:33:43 -0500

    First commit

---
+++ src/main/groovy/com/jos/JugoterapiaApplication.groovy
@@ -0,0 +1,13 @@
+package com.jos.dem.jugoterapia
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class JugoterapiaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run JugoterapiaApplication, args
+	}
+
+}
