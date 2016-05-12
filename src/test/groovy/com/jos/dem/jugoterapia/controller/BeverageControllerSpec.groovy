0fb5c11eb2cd21f329a4edaf24eaa287caa557c8
josdem <joseluis.delacruz@gmail.com>
2016-05-11 20:12:47 -0500

    Completing beverageController spec

--- src/test/groovy/com/jos/dem/jugoterapia/controller/BeverageControllerSpec.groovy
+++ src/test/groovy/com/jos/dem/jugoterapia/controller/BeverageControllerSpec.groovy
@@ -3,6 +3,7 @@ package com.jos.dem.jugoterapia.controller
 import spock.lang.Specification
 
 import com.jos.dem.jugoterapia.model.Category
+import com.jos.dem.jugoterapia.model.Beverage
 import com.jos.dem.jugoterapia.service.BeverageService
 
 class BeverageControllerSpec extends Specification {
@@ -16,14 +17,45 @@ class BeverageControllerSpec extends Specification {
   }
 
   void "should get categories"(){
-    given:"An categories"
-      List<Category> categories = [new Category(id:1,name:'Curativos'), new Category(id:2,name:"Energetizantes")]
+    given:"A categories"
+      Category categoryOne = new Category(id:1,name:'Curativos')
+      Category categoryTwo = new Category(id:2,name:"Energetizantes")
+      List<Category> categories = [categoryOne, categoryTwo]
     when:"We get catetories"
       beverageService.categories() >> categories
       List<Category> result = controller.categories()
-      println result
     then:"We expect categories"
       result.size() == 2
+      result.contains(categoryOne)
+      result.contains(categoryTwo)
+  }
+
+  void "should get beverages by category"(){
+    given:"Two beverages"
+      Beverage beverageOne = new Beverage()
+      Beverage beverageTwo = new Beverage()
+    and:"A categoryId"
+      Long categoryId = 1L
+    when:"We get beverages by category"
+      beverageService.findByCategory(categoryId) >> [beverageOne, beverageTwo]
+      List<Beverage> result = controller.beverages(categoryId)
+    then:
+      result.size() == 2
+      result.contains(beverageOne)
+      result.contains(beverageTwo)
+  }
+
+  void "should get beverage by id"(){
+    given:"A beverageId"
+      Long beverageId = 1L
+    and:"A beverage"
+      Beverage beverage = new Beverage(id:beverageId)
+    when:"We get an beverage by id"
+      beverageService.findById(beverageId) >> beverage
+      Beverage result = controller.beverage(beverageId)
+    then:"We expect beverage"
+     result instanceof Beverage
+     result.id == beverageId
   }
 
 }
