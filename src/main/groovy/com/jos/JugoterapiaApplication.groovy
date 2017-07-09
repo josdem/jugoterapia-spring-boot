f5aa47bc38b3d8e79431d3a08354799c40926b00
josdem <joseluis.delacruz@gmail.com>
2017-07-09 18:55:37 -0500

    #3 Creating war build package

--- src/main/groovy/com/jos/JugoterapiaApplication.groovy
+++ src/main/groovy/com/jos/JugoterapiaApplication.groovy
@@ -17,9 +17,10 @@ package com.jos.dem.jugoterapia
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.web.SpringBootServletInitializer
 
 @SpringBootApplication
-class JugoterapiaApplication {
+class JugoterapiaApplication extends SpringBootServletInitializer {
 
 	static void main(String[] args) {
 		SpringApplication.run JugoterapiaApplication, args
