daa01e9101a215ef03b678ea0715facaae06b2a5
josdem <joseluis.delacruz@gmail.com>
2017-09-17 11:19:36 -0500

    #10 Adding business exception

--- src/main/groovy/com/jos/dem/jugoterapia/service/impl/RestClientImpl.groovy
+++ src/main/groovy/com/jos/dem/jugoterapia/service/impl/RestClientImpl.groovy
@@ -1,26 +1,45 @@
 /*
-  Copyright 2017 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+Copyright 2017 José Luis De la Cruz Morales <joseluis.delacruz@gmail.com>
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 
 package com.jos.dem.jugoterapia.service.impl
 
-import com.jos.dem.jugoterapia.service.RestClient
+import org.springframework.stereotype.Service
 
+import com.jos.dem.jugoterapia.service.RestClient
+import com.jos.dem.jugoterapia.service.BusinessException
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+@Service
 class RestClientImpl implements RestClient {
 
-  def getData(){
+  Logger log = LoggerFactory.getLogger(this.class)
 
+  def getData(){
+    try {
+      log.info "Calling Service"
+      RESTClient restClient = new RESTClient()
+      def result = restClient.get(
+        uri : 'http://jugoterapia.josdem.io/jugoterapia-server/beverage/beverage?beverageId=35'
+      )
+      result.data
+    } catch(Exception ex) {
+      log.warn "Error ${ex.message}"
+      throw new BusinessException(ex.message)
+    }
   }
 
 }
